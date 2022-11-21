65098ba9c65623ab1c849347323f282aab4ed38e
Maximos Nikiforakis <nikiforos@live.co.uk>
2022-11-21 21:01:57 +0200

    create initial users

---
+++ src/main/resources/db/data/create_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users(Name, Surname, Sex, Age, Email, Username, Password, Country, Area, PhoneNumber)
+VALUES('George', 'Papas', 'M', 34, 'g.papas@mymail.com', 'gpap', 'gpap', 'Greece', 'Attiki', '00306973880123');
+INSERT INTO Users(Name, Surname, Sex, Age, Email, Username, Password, Country, Area, PhoneNumber)
+VALUES('Nikos', 'Ioannou', 'M', 23, 'n.ioannou@mymail.com', 'nioan', 'nioan', 'Greece', 'Macedonia', '00306978780126');
+INSERT INTO Users(Name, Surname, Sex, Age, Email, Username, Password, Country, Area, PhoneNumber)
+VALUES('Maria', 'Papadopoulou', 'F', 28, 'm.papadopoulou@mymail.com', 'mpapa', 'mpapa', 'Greece', 'Arta', '00306930019823');
+INSERT INTO Users(Name, Surname, Sex, Age, Email, Username, Password, Country, Area, PhoneNumber)
+VALUES('Jesus', 'Christ', 'M', 33, 'jc@mymail.com', 'jc', 'jc', 'Israel' , 'Palestine', '008091029328');
