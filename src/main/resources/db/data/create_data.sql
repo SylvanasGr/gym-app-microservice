607ed80bbd9f6f35053da9d03aee7b3cba70d575
Maximos Nikiforakis <nikiforos@live.co.uk>
2022-11-24 20:51:00 +0200

    create trainees, trainers in db

--- src/main/resources/db/data/create_data.sql
+++ src/main/resources/db/data/create_data.sql
@@ -6,3 +6,12 @@ INSERT INTO Users(Name, Surname, Sex, Age, Email, Username, Password, Country, A
 VALUES('Maria', 'Papadopoulou', 'F', 28, 'm.papadopoulou@mymail.com', 'mpapa', 'mpapa', 'Greece', 'Arta', '00306930019823');
 INSERT INTO Users(Name, Surname, Sex, Age, Email, Username, Password, Country, Area, PhoneNumber)
 VALUES('Jesus', 'Christ', 'M', 33, 'jc@mymail.com', 'jc', 'jc', 'Israel' , 'Palestine', '008091029328');
+
+INSERT INTO Trainers(UserId, TrainerTypeId, BankAccount, ProfileDescription)
+VALUES(1, 2, 'GR123213912093', 'My name is George Papas.');
+INSERT INTO Trainers(UserId, TrainerTypeId, BankAccount, ProfileDescription)
+VALUES(2, 1, 'GR021371231235', 'My name is Nikos Ioannou.');
+INSERT INTO Trainees(UserId, Weight, Height, Injuries)
+VALUES(3, 56, 1.67, 'Broken leg 4 yrs ago.');
+INSERT INTO Trainees(UserId, Weight, Height, Injuries)
+VALUES(4, 81, 1.78, 'Nailed hands.');
